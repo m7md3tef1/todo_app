586ceb38c0413bd9e4cf82602929ccbfbfa7b45c
mohamed <mnpel789f@gmail.com>
2024-10-11 16:29:43 +0300

    final

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import file_selector_macos
 import flutter_image_compress_macos
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
